84c6da464745956c5d8b648b1deae51457562ae7
wenlong92 <835952234@qq.com>
2022-05-26 17:23:12 +0800

    5.0update

---
+++ 5.0控制结构.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Go提供了条件结构和分支结构
+
+	// if-else结构
+	// switch结构
+	// select结构，用于channel的选择
+
+	// for(range)结构 迭代或循环结构来重复执行一次或多次某段代码
+
+	// break和continue关键字可以用于中途改变循环状态
+
+	// return结束某个函数，或使用goto和标签调整程序的执行位置
+
+	// Go省略了if、switch和for结构中条件语句两侧的括号
+}
