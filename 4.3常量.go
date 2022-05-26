7ee9462ca579149cfb2fc9f37121e391544e74ca
wenlong92 <835952234@qq.com>
2022-05-26 15:17:39 +0800

    4.3update

---
+++ 4.3常量.go
@@ -0,0 +1,19 @@
+package main
+
+const pi = 3.1415926
+
+const b string = "abc"
+
+// 并行赋值
+const beef, two, c = "eat", 2, "veg"
+const (
+	Monday, Tuesday, Wednesday = 1, 2, 3
+	Thursday, Friday, Saturday = 4, 5, 6
+)
+
+// 枚举
+const (
+	Unknown = 0
+	Female = 1
+	Male = 2
+)
