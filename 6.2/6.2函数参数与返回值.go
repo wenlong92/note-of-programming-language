08d1746abf201248a5543602f8825de303375609
wenlong92 <835952234@qq.com>
2022-06-01 18:26:57 +0800

    6.2update

--- 6.2/6.2函数参数与返回值.go
+++ 6.2/6.2函数参数与返回值.go
@@ -1,6 +1,8 @@
 package main
 import (
+	"errors"
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -12,4 +14,23 @@ func main() {
 	// 也可以传递参数地址(引用传递)，函数可以直接修改参数的值，此时传递的是一个指针
 
 	// 几乎任何情况下，传递指针的消耗都比副本小
+
+	// 切片(slice)，字典(map)、接口(interface)、通道(channel)这样的引用类型都是默认使用引用传递
+	demo1(1, 2)
+	MySqrt(2.8)
+}
+
+func demo1(num1, num2 int) (num3, num4, num5 int) {
+	num3 = num1 + num2
+	num4 = num1 * num2
+	num5 = num2 - num1
+	fmt.Printf("%d,%d,%d\n",num3,num4,num5)
+	return
+}
+
+func MySqrt(num1 float64) (float64, error) {
+	if(num1 < 0) {
+		return float64(math.NaN()), errors.New("I won't be able to do a sqrt of negative number!")
+	}
+	return math.Sqrt(num1), nil
 }
