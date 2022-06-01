42f9f9f292281ecb201d1802ae5e868f167a9596
wenlong92 <835952234@qq.com>
2022-06-01 12:02:05 +0800

    update

--- 5.6/5.6标签与goto.go
+++ 5.6/5.6标签与goto.go
@@ -17,5 +17,22 @@ func main() {
 		}
 
 		// 可以使用goto语句和标签配合使用，模拟循环
+		// 尽量不适用goto和标签，如果使用，则goto应当放在标签前面，且goto和标签之间不能出席那定义新变量的语句，否则编译失败
+
+
+
+		i := 0
+		for { //since there are no checks, this is an infinite loop
+			if i >= 3 { break }
+			//break out of this for loop when this condition is met
+			fmt.Println("Value of i is:", i)
+			i++
+		}
+		fmt.Println("A statement just after for loop.")
 		
+
+		for i := 0; i<7 ; i++ {
+			if i%2 == 0 { continue }
+			fmt.Println("Odd:", i)
+		}
 }
