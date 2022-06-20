8e782e2a29b92e95597655eb0f950c773543fc57
wenlong92 <835952234@qq.com>
2022-06-20 15:32:49 +0800

    11.5update

---
+++ the-note-of-the-way-to-go/11.5/11.5测试一个值是否实现了某个接口.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 这是11.3类型断言中的一个特例：假定v是一个值，然后测试它是否实现了Stringer接口，可以这样：
+	type Stringer interface {
+		String() string
+	}
+	if sv, ok := v.(Stringer); ok {
+		fmt.Printf("v implements String(): %s\n", sv.String())
+	}
+	// Print函数就是如此检测类型是否可以打印自身
+
+	// 接口是一种契约，实现类型必须满足它，它描述了类型的行为，规定类型可以做什么。
+	// 接口彻底将类型能做什么，以及如何做分离出来，
+	// 使得相同接口的变量在不同时刻表现出不同的行为，这就是多态的本质
+
+	// 编写参数是接口变量的函数，这使得它们更具有一般性
+
+	// 使用接口使代码更具有普适性
+
+	// 标准库中到处都使用了这个原则。
+}
