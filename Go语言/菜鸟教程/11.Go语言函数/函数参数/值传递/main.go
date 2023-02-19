08e5b06776f353b2a9053b7a5d4e3b31c9e51adb
wenlong92 <835952234@qq.com>
2023-02-19 17:30:57 +0800

    值传递、引用传递

--- Go语言/菜鸟教程/11.Go语言函数/函数参数/值传递/main.go
+++ Go语言/菜鸟教程/11.Go语言函数/函数参数/值传递/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 传递是指在调用函数时将实际参数复制一份传递到函数中，这样在函数中如果对参数进行修改，不会影响到实际参数
+	// 默认情况下，Go语言使用的是值传递
+
+	// 使用值传递调用swap函数
+	var a, b int = 100, 200
+	fmt.Printf("交换前a的值为：%d \n", a)
+	fmt.Printf("交换前b的值为：%d \n", b)
+
+	swap(a, b)
+	fmt.Printf("交换后a的值为：%d \n", a)
+	fmt.Printf("交换后b的值为：%d \n", b)
+	// 程序中使用的是值传递，所以两个值并没有实现交换，可以使用引用传递来实现交换效果
+
+}
+func swap(x, y int) int {
+	var temp int
+	temp = x
+	x = y
+	y = temp
+	return temp
 }
--- Go语言/菜鸟教程/11.Go语言函数/函数参数/引用传递/main.go
+++ Go语言/菜鸟教程/11.Go语言函数/函数参数/引用传递/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 引用传递是指在调用函数时将实际参数的地址传递到函数中，那么在函数中对参数所进行的修改，将影响到实际参数
+	// 引用传递指针参数传递到函数内
+
+	var a, b int = 100, 200
+	fmt.Printf("交换前a的值为：%d \n", a)
+	fmt.Printf("交换前b的值为：%d \n", b)
+	/*调用函数swap
+	&a指向a指针，a变量的地址
+	&b指向b指针，b变量的地址
+	*/
+	swap(&a, &b)
+	fmt.Printf("交换后a的值为：%d \n", a)
+	fmt.Printf("交换后b的值为：%d \n", b)
 }
+
+func swap(x, y *int) {
+	var temp int
+	temp = *x
+	*x = *y
+	*y = temp
+}
+
+// 其他运算符： & 返回变量存储地址，&a将给出变量的实际地址； * 指针变量，*a是一个指针变量；
