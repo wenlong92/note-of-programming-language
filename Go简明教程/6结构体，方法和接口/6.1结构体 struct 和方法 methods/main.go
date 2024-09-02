d4ecf1c9374c5e967aaad23a2cc63e4d6af7a420
wenlong92 <835952234@qq.com>
2024-09-02 10:43:41 +0800

    update

--- Go简明教程/6结构体，方法和接口/6.1结构体 struct 和方法 methods/main.go
+++ Go简明教程/6结构体，方法和接口/6.1结构体 struct 和方法 methods/main.go
@@ -1,1 +1,27 @@
-package __1结构体_struct_和方法_methods
+package main
+
+import "fmt"
+
+// 结构体类似其他语言中的 class，可以在结构体中定义多个字段，为结构体实现方法，实例化等
+// 定义一个结构体 Student，并添加name、age 字段，并实现 hello()方法
+type Student struct {
+	name string
+	age  int
+}
+
+//实现方法和实现函数之间的区别在于 func和函数名之间，加上改方法对应的实例名stu及其类型*Student
+func (stu *Student) hello(person string) string {
+	return fmt.Sprintf("hello %s, I am %s", person, stu.name)
+}
+
+func main() {
+	stu := &Student{
+		name: "Tom",
+	}
+	msg := stu.hello("Jack")
+	fmt.Println(msg)
+
+	//除此之外，还可以使用 new 实例化
+	stu2 := new(Student)
+	fmt.Println(stu2.hello("Alice"))
+}
--- Go简明教程/6结构体，方法和接口/6.2接口 interfaces/main.go
+++ Go简明教程/6结构体，方法和接口/6.2接口 interfaces/main.go
@@ -1,1 +1,35 @@
-package __2接口_interfaces
+package main
+
+import "fmt"
+
+// 一般而言，接口定义了了一组方法的集合，接口不能被实例化，一个类型可以实现多个接口
+// 例子：定义一个接口 Person 和对应的方法 getName()和 getAge()
+type Person interface {
+	getName() string
+}
+
+type Student struct {
+	name string
+	age  int
+}
+
+func (stu *Student) getName() string {
+	return stu.name
+}
+
+type Worker struct {
+	name   string
+	gender string
+}
+
+func (w *Worker) getName() string {
+	return w.name
+}
+
+func main() {
+	var p Person = &Student{
+		name: "Tom",
+		age:  18,
+	}
+	fmt.Println(p.getName())
+}
